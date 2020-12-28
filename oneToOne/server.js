bc144d26150da889c6f4e68de21d8acc5f53c311
wanglihang <1052868843@qq.com>
2020-12-28 21:14:42 +0800

    xx

--- oneToOne/server.js
+++ oneToOne/server.js
@@ -1,21 +1,19 @@
 var ws = require("nodejs-websocket")
 
-//记录连接数
-let count = 0
+let count = 1
 let conns = {}
 var server = ws.createServer((conn) => {
-    count++
-    conn.userName = `用户${count}`
-    broadcast(`${conn.userName}进入了聊天室`)
-    conns[count] = conn
     //接收到客户端数据
-    conn.on('text', (data) => {
-        console.log(data)
-        broadcast(data)
+    conns[count]=conn
+    count++
+    conn.on('text', (e) => {
+        let {toid,data}  = JSON.parse(e)
+        let msg={
+            data:data
+        }
+        conns[toid].send(JSON.stringify(msg))
     })
     conn.on('close', (data) => {
-        count--
-        broadcast(`${conn.userName}离开了聊天室`)
     })
     conn.on('error', (data) => {
         console.log("客户端连接异常")
@@ -26,10 +24,3 @@ server.listen('3000', () => {
     console.log("启动websocket服务成功")
 })
 
-function broadcast(msg) {
-    //给所有用户发送一条消息
-    server.connections.forEach((item) => {
-        item.send(msg)
-    })
-
-}
--- server.js
+++ server.js
@@ -2,12 +2,10 @@ var ws = require("nodejs-websocket")
 
 //记录连接数
 let count = 0
-let conns = {}
 var server = ws.createServer((conn) => {
     count++
     conn.userName = `用户${count}`
     broadcast(`${conn.userName}进入了聊天室`)
-    conns[count] = conn
     //接收到客户端数据
     conn.on('text', (data) => {
         console.log(data)
