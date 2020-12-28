4f9f1352f66f85b4d56ae26464e7edcf0d09a922
wanglihang <wanglihang@iauto.com>
2020-12-28 17:06:59 +0800

    add new file

---
+++ server.js
@@ -0,0 +1,33 @@
+var ws = require("nodejs-websocket")
+
+//记录连接数
+let count = 0
+var server = ws.createServer((conn) => {
+    count++
+    conn.userName = `用户${count}`
+    broadcast(`${conn.userName}进入了聊天室`)
+    //接收到客户端数据
+    conn.on('text', (data) => {
+        console.log(data)
+        broadcast(data)
+    })
+    conn.on('close', (data) => {
+        count--
+        broadcast(`${conn.userName}离开了聊天室`)
+    })
+    conn.on('error', (data) => {
+        console.log("客户端连接异常")
+    })
+})
+
+server.listen('3000', () => {
+    console.log("启动websocket服务成功")
+})
+
+function broadcast(msg) {
+    //给所有用户发送一条消息
+    server.connections.forEach((item) => {
+        item.send(msg)
+    })
+
+}
